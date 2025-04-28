7935363d6910e437d9e4668d53172b59acd0a849
fai7h01 <l.varsimashvil10@gmail.com>
2025-04-28 14:45:03 +0400

    application logs services added

--- src/main/java/com/example/javatutorial/HelloWorldApplication.java
+++ src/main/java/com/example/javatutorial/HelloWorldApplication.java
@@ -1,14 +1,19 @@
 package com.example.javatutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class HelloWorldApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
-        System.out.println("Hello, World!");
+        log.info("Hello World!");
     }
 
 }
